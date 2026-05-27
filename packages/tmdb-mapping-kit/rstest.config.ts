a101de5ef49107b9d98a24683dbed94403aac7a6
Baran Wang <me@baran.wang>
2026-05-28 00:07:59 +0800

    test(tmdb-mapping-kit): load protobuf fixtures in rstest
    Ultraworked with [Sisyphus](https://github.com/code-yeongyu/oh-my-openagent)
    
    Co-authored-by: Sisyphus <clio-agent@sisyphuslabs.ai>

--- packages/tmdb-mapping-kit/rstest.config.ts
+++ packages/tmdb-mapping-kit/rstest.config.ts
@@ -1,6 +1,8 @@
 import { withRslibConfig } from "@rstest/adapter-rslib";
 import { defineConfig } from "@rstest/core";
+import { protobufjsPlugin } from "rsbuild-plugin-protobufjs";
 
 export default defineConfig({
   extends: withRslibConfig(),
+  plugins: [protobufjsPlugin({ wrap: "esm" })],
 });
